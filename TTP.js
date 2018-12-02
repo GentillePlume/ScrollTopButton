0adda2cd0ec49dbbbc48e40ab2c640d26c6c6e3e
Gentille Plume <melvin.lemoine@feather-project.org>
2018-12-02 18:27:29 +0100

    Improved library (work in progress)

--- TTP.js
+++ TTP.js
@@ -1,55 +1,65 @@
-console.log("TTP launched");
+function ttp(box_shape, box_color, icon, icon_color, box_animation, icon_animation) {
 
-// ########## BUILD
-var button = document.createElement("div");
-document.body.appendChild(button);
-button.classList.add("TTP-button");
+  console.log("🎇 To The Top - launched ✔️");
+  console.log("⚙️ config :");
+  console.log("%cbox shape : %c" + box_shape, "color:grey", "color: black; font-weight: bold");
+  console.log("%cbox color : %c" + box_color, "color:grey", "color: black; font-weight: bold");
+  console.log("%cicon : %c" + icon, "color:grey", "color: black; font-weight: bold");
+  console.log("%cicon color : %c" + icon_color, "color:grey", "color: black; font-weight: bold");
+  console.log("%cbox animation : %c" + box_animation, "color:grey", "color: black; font-weight: bold");
+  console.log("%cicon animation : %c" + icon_animation, "color:grey", "color: black; font-weight: bold");
+  console.log("-----------------------");
 
-$(".TTP-button").css({
-  "display": "flex",
-  "position": "fixed",
-  "bottom": "50px",
-  "right": "50px",
-  "width": "100px",
-  "height": "100px",
-  "justify-content": "center",
-  "align-items": "center",
-  "border-radius": "100%",
-  "background-color": "black",
-  "cursor": "pointer",
-  "visibility": "hidden",
-  "opacity": "0",
-  "transition": "opacity .3s ease-in-out 0s"
-});
+  // ########## BUILD
+  var button = document.createElement("div");
+  document.body.appendChild(button);
+  button.classList.add("TTP-button");
 
-var arrow = document.createElement("i");
-button.appendChild(arrow);
+  $(".TTP-button").css({
+    "display": "flex",
+    "position": "fixed",
+    "bottom": "50px",
+    "right": "50px",
+    "width": "100px",
+    "height": "100px",
+    "justify-content": "center",
+    "align-items": "center",
+    "border-radius": "100%",
+    "background-color": "black",
+    "cursor": "pointer",
+    "visibility": "hidden",
+    "opacity": "0",
+    "transition": "opacity .3s ease-in-out 0s"
+  });
 
-arrow.classList.add("fas");
-arrow.classList.add("fa-angle-up");
-arrow.classList.add("TTP-icon");
+  var arrow = document.createElement("i");
+  button.appendChild(arrow);
 
-$(".TTP-icon").css({
-  "font-size": "75px",
-  "color": "white"
-});
+  arrow.classList.add("TTP-icon");
+  $(".TTP-icon").addClass(icon);
 
-// ########## ENGINE
-$(document).scroll(function() {
-  if ($(document).scrollTop() > 0) {
-    $(".TTP-button").css({
-      "visibility": "visible",
-      "opacity": "1"
-    });
+  $(".TTP-icon").css({
+    "font-size": "75px",
+    "color": "white"
+  });
 
-  } else {
-    $(".TTP-button").css({
-      "opacity": "0"
-    });
-    setTimeout(function() {
+  // ########## ENGINE
+  $(document).scroll(function() {
+    if ($(document).scrollTop() > 0) {
       $(".TTP-button").css({
-        "visibility": "hidden"
+        "visibility": "visible",
+        "opacity": "1"
       });
-    }, 300);
-  }
-})
+
+    } else {
+      $(".TTP-button").css({
+        "opacity": "0"
+      });
+      setTimeout(function() {
+        $(".TTP-button").css({
+          "visibility": "hidden"
+        });
+      }, 300);
+    }
+  })
+}
